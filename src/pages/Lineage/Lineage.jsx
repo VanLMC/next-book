6990aa5227c4e1af44ca409f112549da21567f48
Vanderlei <vanderleichagas42@gmail.com>
2020-12-18 23:47:37 -0300

    Pagina Linhagem

--- src/pages/Lineage/Lineage.jsx
+++ src/pages/Lineage/Lineage.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import './lineage.css';
+
+import Bulbassauro from '../../assets/Bulbassauro.jpg' ;
+import Ivissauro from '../../assets/Ivissauro.jpg';
+import Venossauro from '../../assets/venusauro.jpg';
+import Seta from '../../assets/seta.png';
 export default function Lineage() {
+
+        const [evolutions, setEvolutions] = useState([
+            {
+                name: 'Bulbassauro',
+                image: Bulbassauro,
+            },
+            {
+                name: 'Ivissauro',
+                image: Ivissauro
+            },
+            {
+                name: 'Venossauro',
+                image: Venossauro
+            }
+
+        ]);
+
+
     return (
-        <div>
-            <h1>Linhagem</h1>
+        <div id="lineage">
+
+        <div className="evolution-list">
+            {evolutions.map( evolution => (
+                <>
+
+                <div className="image-container">
+                    <img src={evolution.image} alt={evolution.name} />
+                    
+                    
+                    <figcaption>{evolution.name}</figcaption>
+                </div>
+                <img className="seta" src={Seta} alt=""/>
+                </>
+            ))}
+        </div>
+        
         </div>
     )
 }
